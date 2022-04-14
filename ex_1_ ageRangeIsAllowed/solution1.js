94ff38e86adc71e99db9495d16a27073ad8ebc23
lazalajochy <lazala1401jochy@outlook.com>
2022-04-14 11:51:57 -0400

    update

--- ex_1_ ageRangeIsAllowed/solution1.js
+++ ex_1_ ageRangeIsAllowed/solution1.js
@@ -2,7 +2,7 @@ var ageOne = 25,
   ageTwo = 29;
 
 function ageRangeIsAllowed(ageOne, ageTwo) {
-  return Math.abs(ageOne - ageTwo <= 5) ? true : false
+  return Math.abs(ageOne - ageTwo) <= 5;
 
 }
 
