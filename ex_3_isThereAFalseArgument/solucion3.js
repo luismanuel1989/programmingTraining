8b37d27350bd506cf7610942d0ab0602579a2606
lazalajochy <lazala1401jochy@outlook.com>
2022-04-19 21:30:32 -0400

    solucion3

---
+++ ex_3_isThereAFalseArgument/solucion3.js
@@ -0,0 +1,6 @@
+var argOne = true, argTwo = true, argThree = false;
+isThereAFalseArgument(argOne, argTwo, argThree);
+
+function isThereAFalseArgument(argOne,argTwo, argThree){
+    return argOne !== argTwo || argOne !== argThree || argTwo !== argThree
+  }
