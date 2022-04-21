52f3b83606d46a52f7d3c664a7f4c43e6783fa78
lazalajochy <lazala1401jochy@outlook.com>
2022-04-21 10:04:58 -0400

    solucion3

--- ex_3_isThereAFalseArgument/solucion3.js
+++ ex_3_isThereAFalseArgument/solucion3.js
@@ -2,5 +2,5 @@ var argOne = true, argTwo = true, argThree = false;
 isThereAFalseArgument(argOne, argTwo, argThree);
 
 function isThereAFalseArgument(argOne,argTwo, argThree){
-  return !argOne, !argTwo, !argThree;
+  return !argOne || !argTwo || !argThree;
   }
