f208f720ea933a42f0f8b7d3b96220cb034b7cae
lazalajochy <lazala1401jochy@outlook.com>
2022-05-12 09:58:34 -0400

    evaluateMaterialConditionalAndExclusiveOr

--- ex_7_evaluateMaterialConditionalAndExclusiveOr/solution7.1.js
+++ ex_7_evaluateMaterialConditionalAndExclusiveOr/solution7.1.js
@@ -1,17 +1,22 @@
-var argOne = true, argTwo = false;
+var argOne = true,
+  argTwo = false;
 
-var material_Conditional = materialConditional(argOne, argTwo), exclusive_Or = exclusiveOR(argOne, argTwo);
+var material_Conditional = materialConditional(argOne, argTwo),
+  exclusive_Or = exclusiveOR(argOne, argTwo);
 
-function materialConditional(argOne, argTwo){
-    return !argOne || argTwo;
+function materialConditional(argOne, argTwo) {
+  return !argOne || argTwo;
 }
 
-function exclusiveOR(argOne, argTwo){
-    return argOne != argTwo;
+function exclusiveOR(argOne, argTwo) {
+  return argOne != argTwo;
 }
 
 evaluateMaterialConditionalAndExclusiveOr(material_Conditional, exclusive_Or);
 
-function evaluateMaterialConditionalAndExclusiveOr(material_Conditional, exclusive_Or){
-    return material_Conditional && exclusive_Or;
+function evaluateMaterialConditionalAndExclusiveOr(
+  material_Conditional,
+  exclusive_Or
+) {
+  return material_Conditional && exclusive_Or;
 }
