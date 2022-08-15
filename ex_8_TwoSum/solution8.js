f579247a38edc67e4ac21ec2fd23e2f48df1d870
lazalajochy <lazala1401jochy@outlook.com>
2022-08-15 18:48:42 -0400

    soluction fixed

--- ex_8_TwoSum/solution8.js
+++ ex_8_TwoSum/solution8.js
@@ -2,9 +2,9 @@ var target = 9, nums = [2, 7, 11, 15];
 sumTwo(target, nums);
 
 function sumTwo(target, nums) {
-    var result = [], i, j;
-    for (i = 0; i < nums.length; i++) {
-        for (j = 0; j < nums.length; j++) {
+    var result = [];
+    for (var i = 0; i < nums.length; i++) {
+        for (var j = 0; j < nums.length; j++) {
             if (j !== i) {
                 if (nums[i] + nums[j] == target) {
                     if (!result.includes(i) && !result.includes(j)) result.push(i, j)
